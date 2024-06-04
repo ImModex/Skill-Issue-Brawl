9034e3f89c9468f6093dee82b7906349e0a53518
ImModex <Spatz343@gmail.com>
2024-06-04 23:04:08 +0200

    Added healthbar UI and spell selection slots (experimental)

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthScript : MonoBehaviour
 {
+    public Statscript Statscript;
+    public Slider HealthBar;
+    public TextMeshProUGUI HealthBarText;
+    
     // Start is called before the first frame update
     void Start()
     {
+        HealthBar.value = 1;
+        UpdateHealthBarText();
         
+        Debug.Log(Statscript.maxHealth);
+        Debug.Log(Statscript.currentHealth);
     }
 
     // Update is called once per frame
@@ -15,9 +25,33 @@ public class HealthScript : MonoBehaviour
     {
         
     }
+    
+    private float ScaleHealthToHealthBar()
+    {
+        return Statscript.currentHealth / Statscript.maxHealth;
+    }
+
+    private void UpdateHealthBarText()
+    {
+        HealthBarText.text = $"{Statscript.currentHealth} / {Statscript.maxHealth}";
+    }
 
     public void Damage(int damage)
     {
+        Debug.Log("I am here");
+        Debug.Log(gameObject.name);
+        Debug.Log(gameObject.tag);
+
+        Statscript.currentHealth -= damage;
+
+        HealthBar.value = ScaleHealthToHealthBar();
+        UpdateHealthBarText();
+    
+        if (HealthBar.value <= 0)
+        {
+            Debug.Log("You died lmao");
+        }   
+        
         Debug.Log(damage + "was Taken");
     }
 }
